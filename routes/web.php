7273a096f2fb25ed575f6afa3ab4bfa5d056c17b
afcanop <afcanop@gmail.com>
2020-02-23 17:23:58 -0500

    login

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
 Route::prefix('admin')->group(function () {
     Route::prefix('academic')->group(function () {
         Route::get('student/{codigoCurso}', 'academic\StudentController@create')->name('student.create');
@@ -29,3 +33,7 @@ Route::prefix('admin')->group(function () {
 
     });
 });
+
+
+
+
